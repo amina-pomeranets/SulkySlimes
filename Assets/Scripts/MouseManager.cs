0a4741a5721df3899126387dfd5ed98ba328ab08
amina-pomeranets <166169032+amina-pomeranets@users.noreply.github.com>
2024-04-19 16:35:55 -0400

    Slime Follows Mouse While We Launch
    Wasn't working before because there was a line of code that allowed it to work, but it was commented out.

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MouseManager : MonoBehaviour
 {
-
+    
     [Header("Mouse Info")]
     public Vector3 clickStartLocation;
 
-    [Header ("Physics")]
+    [Header("Physics")]
     public Vector3 launchVector;
     public float launchForce;
 
@@ -16,17 +18,31 @@ public class MouseManager : MonoBehaviour
     public Transform slimeTransform;
     public Rigidbody slimeRigidbody;
 
-    private Vector3 launchForced = new Vector3(0f, 8f);
+    //public float startingX = 8;
+    //public float startingY = 1;
+    //public float startingZ = 0;
+
+    private Vector3 originalSlimePosition;
+    private Quaternion originalSlimeRotation;
+
+    private void Start()
+    {
+        originalSlimePosition = slimeTransform.position;
+        originalSlimeRotation = slimeTransform.rotation;
+    }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) 
+
+        if (Input.GetMouseButtonDown(0))
         {
             clickStartLocation = Input.mousePosition;
         }
 
-        if (Input.GetMouseButton(0)) 
+        if (Input.GetMouseButton(0))
         {
             Vector3 mouseDifference = clickStartLocation - Input.mousePosition;
             launchVector = new Vector3(
@@ -34,14 +50,117 @@ public class MouseManager : MonoBehaviour
                 mouseDifference.y * 1.2f,
                 mouseDifference.y * 1.5f
             );
+            slimeTransform.position = originalSlimePosition - launchVector / 400;
             launchVector.Normalize();
+
+            //slimeTransform.position = new Vector3(startingX, startingY, startingZ);
         }
 
         if (Input.GetMouseButtonUp(0))
         {
             slimeRigidbody.isKinematic = false;
-            slimeRigidbody.AddForce(launchForced, ForceMode.Impulse);
+            slimeRigidbody.AddForce(launchVector * launchForce, ForceMode.Impulse);
+        }
 
+        if (Input.GetMouseButtonDown(1)) 
+        {
+            slimeTransform.position = originalSlimePosition;
+            slimeTransform.rotation = originalSlimeRotation;
         }
     }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+        //if (other.gameObject.tag == "Ground")
+        //{
+            //slimeTransform.position = new Vector3(startingX, startingY, startingZ);
+        //}
+    //} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //public class MouseManager : MonoBehaviour
+    //{
+
+
+
+    /*
+
+        [Header("Mouse Info")]
+        public Vector3 clickStartLocation;
+
+        [Header("Physics")]
+        public Vector3 launchVector;
+        public float launchForce;
+
+        [Header("Slime")]
+        public Transform slimeTransform;
+        public Rigidbody slimeRigidbody;
+
+
+        private Vector3 orignalSlimePosition;
+        private Quaternion originalSlimeRotation;
+
+        //[Header("Lives")]
+       // public LivesManager livesManager;
+
+        void Start()
+        {
+            orignalSlimePosition = slimeTransform.position;
+            originalSlimeRotation = slimeTransform.rotation;
+        }
+
+        void Update()
+        {
+            //if (livesManager.lives < 0)
+           // {
+           //     return;
+           // }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                clickStartLocation = Input.mousePosition;
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 mouseDifference = clickStartLocation - Input.mousePosition;
+                launchVector = new Vector3(
+                    mouseDifference.x,
+                    mouseDifference.y * 1.2f,
+                    (mouseDifference.y) * 1.5f
+                );
+
+                slimeTransform.position = orignalSlimePosition - launchVector / 400;
+                launchVector.Normalize();
+
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                slimeRigidbody.isKinematic = false;
+                slimeRigidbody.AddForce(launchVector * launchForce, ForceMode.Impulse);
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                slimeRigidbody.isKinematic = true;
+                slimeTransform.position = orignalSlimePosition;
+                slimeTransform.rotation = originalSlimeRotation;
+                //livesManager.RemoveLife();
+            }
+        }
+    }
+    */
 }
